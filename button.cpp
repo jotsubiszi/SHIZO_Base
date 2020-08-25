2787e81fbd041ff332ba1972f059c42fd2cae3dc
PanPaskuda <PanPaskuda+github@gmail.com>
2020-08-25 14:10:59 -0700

    feat(button): add basic features like: read state, and debouncing
    REQF2 Button events
        REQF2.1 detect buton press
        REQF2.2 detect button release
        REQF2.3 detect shortpress
        REQF2.4 detect longress
        REQF2.5 detect super-longress
        REQF2.7 debouncing

--- button.cpp
+++ button.cpp
@@ -1,1 +1,157 @@
+#include "gpio.h"
+#include <Arduino.h>
 #include "button.h"
+
+
+static void dummy(void){
+    static uint8_t i;
+        Serial.print("DUMMY:");
+        Serial.println(i++, DEC);
+}
+
+//TODO temporary dummy event arrays to delete
+Button_Callback dummyarray[] = {dummy, dummy, dummy, dummy, dummy, dummy};
+Button_Struct buttonStateMatrix[/*do not set array size explicitly (checked by assert)*/] = {
+    {GPIO_PIN_BUTTON, BUTTON_STATE_SHORT_RELEASED, false, 0, dummyarray},
+};
+
+
+static const uint32_t lastDebounceTime = 0;
+static const uint16_t debounceDelay = 20;
+static const uint16_t longPressTime = 1000u;
+static const uint16_t superLongPressTime = 4000uL;
+
+
+static inline bool isDebounced(Button_Struct *button){
+    uint32_t timePoint = button->pressedTimePoint;
+    return (millis() - timePoint) > debounceDelay;
+}
+
+
+static bool buttonReleasedStateMachine(Button_Struct *button){
+
+    bool isStateChanged = false;
+    Button_State state = button->logicState;
+
+    switch(state){
+        case BUTTON_STATE_SHORT_PRESS:
+
+            state = BUTTON_STATE_SHORT_RELEASED;
+            break;
+
+        case BUTTON_STATE_LONG_PRESS:
+            state = BUTTON_STATE_LONG_RELEASED;
+            break;
+
+        case BUTTON_STATE_SUPERLONG_PRESS:
+            state = BUTTON_STATE_SUPERLONG_RELEASED;
+            break;
+
+        case BUTTON_STATE_SHORT_RELEASED:
+        case BUTTON_STATE_LONG_RELEASED:
+        case BUTTON_STATE_SUPERLONG_RELEASED:
+        case BUTTON_STATE_SENTINEL:
+        default:
+            /* this is the last state */
+            /* do nothing */
+            break;
+    }
+
+    if(state != button->logicState){
+        isStateChanged = true;
+        button->logicState = state;
+        Serial.print("Button no:");
+        Serial.print(button->pinName, DEC);
+        Serial.print(" state:");
+        Serial.println(state, DEC);
+    }
+
+    return isStateChanged;
+}
+
+
+static bool buttonPressedStateMachine(Button_Struct *button){
+    bool isStateChanged = false;
+    Button_State state = button->logicState;
+    uint32_t pressedTime = millis() - button->pressedTimePoint;
+
+    switch(state){
+        case BUTTON_STATE_SHORT_PRESS:
+            if(pressedTime > longPressTime){
+                state = BUTTON_STATE_LONG_PRESS;
+            }
+            break;
+
+        case BUTTON_STATE_LONG_PRESS:
+            if(pressedTime > superLongPressTime){
+                state = BUTTON_STATE_SUPERLONG_PRESS;
+            }
+            break;
+
+        case BUTTON_STATE_SUPERLONG_PRESS:
+            /* this is the last state */
+            /* do nothing */
+            break;
+
+        case BUTTON_STATE_SHORT_RELEASED:
+        case BUTTON_STATE_LONG_RELEASED:
+        case BUTTON_STATE_SUPERLONG_RELEASED:
+        case BUTTON_STATE_SENTINEL:
+        default:
+            state = BUTTON_STATE_SHORT_PRESS;
+            break;
+    }
+
+    if(state != button->logicState){
+        isStateChanged = true;
+        button->logicState = state;
+        Serial.print("Button no:");
+        Serial.print(button->pinName, DEC);
+        Serial.print(" state:");
+        Serial.println(state, DEC);
+    }
+
+    return isStateChanged;
+}
+
+
+static void executeButtonCallback(Button_Struct *button){
+    Button_Callback callback;
+    callback = *button->buttonCallback[button->logicState];
+
+    if(callback != NULL){
+        Serial.print(" callback pin:");
+        Serial.println(button->pinName, DEC);
+        callback();
+    }else{
+        Serial.print("");
+    }
+
+}
+
+
+void buttonTask(void){
+    for(uint8_t i=0; i < (sizeof(buttonStateMatrix)/sizeof(buttonStateMatrix[0])); i++){
+        Button_Struct * button = &buttonStateMatrix[i];
+
+        bool newPhysState = Gpio_GetPinState(button->pinName);
+        if(newPhysState != button->physState){
+            button->pressedTimePoint = millis();
+            button->physState = newPhysState;
+        }
+
+        bool isStateChanged = false;
+        if(isDebounced(button)){
+            if (button->physState == HIGH) {
+                isStateChanged = buttonPressedStateMachine(button);
+            }else{
+                isStateChanged = buttonReleasedStateMachine(button);
+            }
+        }
+
+        if(isStateChanged){
+            executeButtonCallback(button);
+        }
+    }
+}
+
--- button.h
+++ button.h
@@ -2,5 +2,36 @@
 #if !defined BUTTON_H_GUARD
 #define BUTTON_H_GUARD
 
+
+#include "gpio.h"
+
+
+typedef enum {
+    BUTTON_STATE_SHORT_RELEASED,
+    BUTTON_STATE_SHORT_PRESS,
+    BUTTON_STATE_LONG_PRESS,
+    BUTTON_STATE_LONG_RELEASED,
+    BUTTON_STATE_SUPERLONG_PRESS,
+    BUTTON_STATE_SUPERLONG_RELEASED,
+    BUTTON_STATE_SENTINEL,
+} Button_State;
+
+
+typedef void (*Button_Callback)(void);
+
+
+typedef struct{
+    const Gpio_Pin pinName;
+    Button_State logicState;
+    bool physState;
+    uint32_t pressedTimePoint;
+    Button_Callback *buttonCallback[BUTTON_STATE_SENTINEL];
+} Button_Struct;
+
+
+void buttonTask(void);
+
+
+
 #endif
 
